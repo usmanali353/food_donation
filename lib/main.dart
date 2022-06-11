6692c1bfdd1d2d1cd57cd6ed65bf3706f6c3237c
Usman Ali <iib.tech357@hotmail.com>
2022-06-11 16:55:12 +0500

    Added Shimmer effect and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,6 @@ class _MyAppState extends State<MyApp> {
   void initialization() async {
     FlutterNativeSplash.remove();
   }
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
